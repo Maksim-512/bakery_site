75a63f2557c1d04f6b91fd2c731a03abd18cab05
Maksim-512 <maksersh2002@yandex.ru>
2024-08-04 19:38:37 +0300

    update registration

--- bakery_reg/urls.py
+++ bakery_reg/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.main, name='main'),
-    path("register", views.register, name='register'),
+    # path("register", views.register, name='register'),
+    path('register/', views.CustomRegistrationView.as_view(), name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='bakery_reg/logout.html'), name='logout'),
 ]
--- bakery_reg/views.py
+++ bakery_reg/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 from .forms import RegistrationForm, LoginForm
 from django.contrib import messages
 
@@ -10,17 +12,29 @@ def main(request):
                   'bakery_reg/main.html')
 
 
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Создан аккаунт {username}!')
-            return redirect('login')
-    else:
-        form = RegistrationForm()
-    return render(request, 'bakery_reg/register.html', {'form': form})
+# def register(request):
+#     if request.method == 'POST':
+#         form = RegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Создан аккаунт {username}!')
+#             return redirect('login')
+#     else:
+#         form = RegistrationForm()
+#     return render(request, 'bakery_reg/register.html', {'form': form})
+
+class CustomRegistrationView(CreateView):
+    form_class = RegistrationForm
+    template_name = 'bakery_reg/register.html'
+    extra_context = {'title': 'Регистрация на сайте'}
+
+    def get_success_url(self):
+        return reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        return super().form_valid(form)
 
 
 class CustomLoginView(LoginView):
@@ -30,7 +44,6 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('main')
-
 
 # def login(request):
 #     return render(request, 'bakery_reg/login.html', {'form': form})
